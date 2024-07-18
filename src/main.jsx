a12e82e167e17018f34a748d4e3f1a350f5236a1
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-18 23:42:57 +0500

    PERSISTOR

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import store from './store/store'; // Adjust the path as necessary
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import store, { persistor } from './store/store.jsx'; // Adjust the path as necessary
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
-  </Provider>
-);
+  </PersistGate>
+</Provider>,
+)
